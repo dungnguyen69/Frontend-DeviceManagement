35de0d9435aef59487ea6ce6b8c1a421b0bf3a93
DESKTOP-HTPDBCF\ROG Zephyrus <dungnc69.420@gmail.com>
2023-07-18 03:30:25 +0700

    Configure for deployment

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+};
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/Frontend-DeviceManagement'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
-);
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/Frontend-DeviceManagement/index.html'));
+});
 
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', { root: 'dist/angular-heroku/' }),
+);
+app.use(forceSSL());
 app.listen(process.env.PORT || 8080);
