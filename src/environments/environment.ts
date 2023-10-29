a95663c6537bb72b7b3a843b9b8621ba9a0ed1f8
DESKTOP-HTPDBCF\ROG Zephyrus <dungnc69.420@gmail.com>
2023-10-29 21:15:22 +0700

    Fixed environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: "ec2-18-141-43-16.ap-southeast-1.compute.amazonaws.com:8080"
+    apiUrl: "ec2-18-141-43-16.ap-southeast-1.compute.amazonaws.com:8080/"
 };
